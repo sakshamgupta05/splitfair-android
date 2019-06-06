3f3ca3356ea2ff7f201ffe4c3002513cf85633bf
Saksham Gupta <sakshamgupta05@gmail.com>
2019-06-07 01:40:23 +0530

    create login activity

---
+++ app/src/main/java/in/splitfair/LoginActivity.kt
@@ -0,0 +1,14 @@
+package `in`.splitfair
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import com.google.firebase.auth.PhoneAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
